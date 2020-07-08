e6506dd577429115b1ec50a44244b8ed57e35aaf
Kyle Jordan <kjord021@fiu.edu>
2020-07-08 07:46:21 -0400

    Add Organization and Style

--- src/app.js
+++ src/app.js
@@ -13,23 +13,38 @@ app.get("/", function(req, res){
 
 });
 
+app.get("/index.html", function(req, res){
+
+  res.sendFile(__dirname + "/index.html")
+
+});
+
 app.post("/", function(req, res) {
   const searchText = req.body.movieName;
 
   const apikey = "f00dfae4";
-  const url = ("https://www.omdbapi.com/?apikey=f00dfae4&s=" + searchText);
+  const url = ("https://www.omdbapi.com/?apikey="+ apikey +"&s=" + searchText);
 
   fetch(url)
   .then(response => response.json())
   .then(data =>{
     console.log(data);
 
+    res.write("<body style='background-color:#222'> <br>")
+
     for (i = 0; i < data.Search.length; i++){
-      res.write("<div class='container'><center> <h3>" + data.Search[i].Title + "</h3> </center>");
-      res.write("<center><button><img src=" + data.Search[i].Poster + "></center></button>");
+
+      const movieTitle = data.Search[i].Title;
+      const movieID = data.Search[i].imdbID;
+      const movieImage = data.Search[i].Poster;
+      const movieURL = ("http://www.omdbapi.com/?apikey="+ apikey + "&i=" + movieID + "&plot=full")
+
+
+      res.write("<div class='container'><center> <h3 style='color:white'>" + movieTitle + "</h3> </center>");
+      res.write("<center><button><img src=" + movieImage + "></center></button>");
 
     }
-    res.write("</div>")
+    res.write("</div></body>")
     res.send();
   });
 
