373c4e85ae6daae04ad8703e0eb4bf0aa8732aa1
Kyle Jordan <kjord021@fiu.edu>
2020-07-11 13:11:24 -0400

    Finish Restructuring with EJS

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const https = require("https");
 const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
 const ejs = require("ejs");
-const _ = require("lodash");
 
 const app = express();
 const movies = [];
@@ -21,19 +20,11 @@ app.get("/", function(req, res) {
   res.render("index", {
     movies: movies
   });
-  //
-  // while (movies.length > 0) {
-  //   movies.pop();
-  // }
 
 });
 
 
 app.post("/", function(req, res) {
-
-  // while (movies.length > 0) {
-  //   movies.pop();
-  // }
 
   const searchText = req.body.movieName;
 
@@ -59,13 +50,6 @@ app.post("/", function(req, res) {
             id: movieID,
             image: movieImage,
             url: movieURL,
-            // year: yearReleased,
-            // genre: genre,
-            // runtime: runtime,
-            // director: director,
-            // actors: actors,
-            // plot: plot,
-            // rating: rating
           };
           movies.push(movie);
 
@@ -80,34 +64,51 @@ app.post("/", function(req, res) {
 
 app.get("/movies/:postName", function(req, res) {
 
+  const requestedTitle = req.params.postName;
 
-  const requestedTitle = _.lowerCase(req.params.postName);
+  const movieInformation = {
+    title: "",
+    yearReleased: "",
+    runtime: "",
+    genre: "",
+    director: "",
+    actors: "",
+    plot: "",
+    rating: "",
+    image: ""
+  };
 
-  movies.forEach(function(movie) {
-    if (_.lowerCase(movie.id) === requestedTitle) {
+  for (var i = 0; i < movies.length; i++) {
+    if (movies[i].id === requestedTitle) {
 
-      const movieURL = ("http://www.omdbapi.com/?apikey=" + apikey + "&i=" + movie.movieID + "&plot=full")
+      const movieURL = ("http://www.omdbapi.com/?apikey=" + apikey + "&i=" + movies[i].id + "&plot=full")
+
+      movieInformation.title = movies[i].title;
+      movieInformation.image = movies[i].image;
 
       fetch(movieURL)
         .then(resp => resp.json())
         .then(movieData => {
-          console.log("Found movie by ID " + movie.movieID + " retrieving results.");
+          console.log("Found movie by ID " + requestedTitle + " retrieving results.");
+          movieInformation.yearReleased = movieData.Year;
+          movieInformation.runtime = movieData.Runtime;
+          movieInformation.genre = movieData.Genre;
+          movieInformation.director = movieData.Director;
+          movieInformation.actors = movieData.Actors;
+          movieInformation.plot = movieData.Plot;
+          movieInformation.rating = movieData.imdbRating;
 
-          const yearReleased = movieData.Year;
-          const runtime = movieData.Runtime;
-          const genre = movieData.Genre;
-          const director = movieData.Director;
-          const actors = movieData.Actors;
-          const plot = movieData.Plot;
-          const rating = movieData.imdbRating;
+          res.render("movie", {
+            movieInformation: movieInformation
+          });
 
         });
     }
-  });
-  res.render("movie", {
-
-  });
+  }
 });
+
+
+
 
 app.get("/404", function(req, res) {
   res.render("404", {});
